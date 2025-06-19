8cfeda637a1364cc8bac37f785159653c568afad
dedi <id.dedi81@gmail.com>
2025-06-19 20:58:04 +0700

    setup husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,24 +5,23 @@ import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
+    ignores: ['**/build/**', '**/node_modules/**', '**/public/**']
+  },
+  {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    ignores: ["**/build/*", "**/node_modules/*", "**/public/*"],
     plugins: {
       '@typescript-eslint': tseslint.plugin,
       js
     },
-    extends: ['js/recommended'],
+    extends: ['js/recommended']
   },
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: {
       parser: tseslint.parser,
-      // parserOptions: {
-      //   ecmaVersion: "latest",
-      //   sourceType: "module",
-      // },
       globals: globals.node
-    },
+    }
   },
-  tseslint.configs.recommended,
-]);
+  ...tseslint.configs.recommended
+])
+
